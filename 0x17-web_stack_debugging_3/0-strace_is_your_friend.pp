a20302df93aabdf97c642e51b6dafabfda06e915
Taofeeq Ogunsanya <taofeeq.muhammd22@gmail.com>
2023-11-08 16:23:01 +0000

    Task updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
- a faulty wordpress site
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
 exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-/var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
