46984122bbcb7a7dfe2d5386c85a7b1ed329c55a
Taofeeq Ogunsanya <taofeeq.muhammad22@gmail.com>
2024-01-16 14:40:25 -0800

    file modified

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
+        "Accept": "application/json"
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
